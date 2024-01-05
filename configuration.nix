a3b5638e3e7e765a0daac09cefdb626e9f0c8b0a
remi <remi@dupre.io>
2024-01-05 15:31:11 +0100

    enable ssh

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,8 @@
       home-manager.nixosModules.home-manager
     ];
 
+  nix.sshServe.enable = true;
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   security.polkit.enable = true;
 
